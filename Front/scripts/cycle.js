70f1bc91139923f31197c6d9d06343bcc16b53a9
Ivan Gavrilov <145210392+LiquidSubstance@users.noreply.github.com>
2026-03-03 11:24:40 +0300

    Обновил смену заднего плана
    Убрал странную реализацию, зависящую от фреймрейта

--- Front/scripts/cycle.js
+++ Front/scripts/cycle.js
@@ -2,16 +2,20 @@ window.addEventListener('load', () => {
     const header = document.querySelector('.header');
     let frame = 1;
     let i = 0;
-    const image_list = ["/contents/renderallv3.png", "/contents/renderallv2.png"];
-    function change_background() {
-        frame++;
-        if (frame == 300) {
-            frame = 0;
-            header.style.background = "linear-gradient(90deg, rgba(255,255,255, 0.2), rgba(255,255,255,0)), url(" + image_list[i] + ")"
-            i++;
-            i %= 2;
-        }
-        requestAnimationFrame(change_background);
-    }
-    change_background();
+    let image_list = [
+        "/contents/background_images/background1.png",
+        "/contents/background_images/background2.png",
+        "/contents/background_images/background3.png",
+        "/contents/background_images/background4.png",
+    ];
+    let n = image_list.length;
+    image_list.forEach(src => {
+        const img = new Image();
+        img.src = src;
+    });
+    console.log(image_list);
+    setInterval(() => {
+        header.style.backgroundImage = "linear-gradient(90deg, rgba(255,255,255, 0.2), rgba(255,255,255,0)), url(" + image_list[i] + ")";
+        i = (i + 1) % n;
+    }, 3000);
 })
