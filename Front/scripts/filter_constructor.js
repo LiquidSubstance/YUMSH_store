a4f460cf24c44c3037c7af3a30b7a195cffdbf08
Ivan Gavrilov <145210392+LiquidSubstance@users.noreply.github.com>
2026-03-10 23:32:59 +0300

    Добавил проверку на наличие обязательных атрибутов

--- Front/scripts/filter_constructor.js
+++ Front/scripts/filter_constructor.js
@@ -32,21 +32,31 @@ function upload_filter() {
     const form_type_filter = document.getElementById("type-select");
     const form_comparison_filter = document.getElementById("comparison");
     const form_name_filter = document.getElementById("select-name");
-    const preview_filter = document.getElementById("preview-filter-item");
-    const label_filter = document.getElementById("preview-filter-item-label");
-    let new_filter = preview_filter.cloneNode(true);
-    let new_label = label_filter.cloneNode(true);
-    new_label.removeAttribute("id");
-    const new_id = "by-" + form_comparison_filter.value + "-" + form_name_filter.value;
-    new_filter.id = new_id;
-    new_label.setAttribute("for", new_id)
-    new_filter.className = "filter-item";
-    new_filter.dataset.type = form_type_filter.value;
-    new_filter.dataset.content = form_name_filter.value;
-    new_filter.dataset.property = form_comparison_filter.value;
-    new_filter.dataset.compareby = "equal";
-    filter_wrapper.append(new_filter);
-    filter_wrapper.append(new_label);
-    filters.push(new Filter(new_filter.dataset.type, new_filter.dataset.content, new_filter.dataset.property, new_filter.dataset.compareby));
-    raw_filters.push(new_filter);
+    if (!form_comparison_filter.value || !form_type_filter.value || !form_name_filter.value) {
+        const upload_button = document.querySelector(".upload-button");
+        upload_button.style.color = "red";
+        upload_button.textContent = "Не удалось загрузить фильтр, отсутствуют обязательные атрибуты."
+        setTimeout(() => {
+            upload_button.style.color = "black";
+            upload_button.textContent = "Загрузить"
+        }, 1000)
+    } else {
+        const preview_filter = document.getElementById("preview-filter-item");
+        const label_filter = document.getElementById("preview-filter-item-label");
+        let new_filter = preview_filter.cloneNode(true);
+        let new_label = label_filter.cloneNode(true);
+        new_label.removeAttribute("id");
+        const new_id = "by-" + form_comparison_filter.value + "-" + form_name_filter.value;
+        new_filter.id = new_id;
+        new_label.setAttribute("for", new_id)
+        new_filter.className = "filter-item";
+        new_filter.dataset.type = form_type_filter.value;
+        new_filter.dataset.content = form_name_filter.value;
+        new_filter.dataset.property = form_comparison_filter.value;
+        new_filter.dataset.compareby = "equal";
+        filter_wrapper.append(new_filter);
+        filter_wrapper.append(new_label);
+        filters.push(new Filter(new_filter.dataset.type, new_filter.dataset.content, new_filter.dataset.property, new_filter.dataset.compareby));
+        raw_filters.push(new_filter);
+    }
 }
