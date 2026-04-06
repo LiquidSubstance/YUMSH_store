f8ffb9e2ca36946208f35b52e492559b2ea0aa91
Ivan Gavrilov <145210392+LiquidSubstance@users.noreply.github.com>
2026-04-07 00:21:08 +0300

    Фикс путей

--- Front/scripts/upload.js
+++ Front/scripts/upload.js
@@ -1,73 +1,49 @@
-const form_name = document.getElementById("file-upload-name");
-const preview_name = document.getElementById("preview-name");
-const form_price = document.getElementById("file-upload-price");
-const preview_price = document.getElementById("preview-price");
-const form_image = document.getElementById("file-upload-image");
-const preview_image = document.getElementById("preview-image");
-const form_description = document.getElementById("page-constructor-text");
-const preview_page_image = document.querySelector(".main-image")
-const preview_description = document.querySelector(".item-description");
-const preview_page_price = document.querySelector(".item-price");
-const preview_page_name = document.querySelector(".item-name");
-form_image.addEventListener("change", () => {
-    let new_image = form_image.files[0];
-    preview_image.src = URL.createObjectURL(new_image);
-    preview_page_image.src = URL.createObjectURL(new_image);
-});
-form_name.addEventListener("change", () => {
-    preview_name.textContent = form_name.value;
-    preview_page_name.textContent = form_name.value;
-});
-form_price.addEventListener("change", () => {
-    preview_price.textContent = form_price.value + "₽";
-    preview_page_price.textContent = form_price.value + "₽";
-});
-form_description.addEventListener("change", () => {
-    preview_description.textContent = form_description.value;
-})
-async function upload_item() {
-    wrapper = document.querySelector(".catalogue");
-    const form_date = document.getElementById("file-upload-date")
-    const form_type = document.getElementById("file-upload-type");
-    const preview_item = document.getElementById("preview");
-    const button = document.getElementById("add-item-button");
-    if (!form_date.value || !form_price.value || !form_name.value || !form_image.value || !form_type.value) {
-        button.textContent = "Не удалось загрузить товар, отсутствуют обязательные атрибуты.";
-        button.style.color = "red";
-        setTimeout(() => {
-            button.textContent = "Загрузить";
-            button.style.color = "black";
-        }, 1000)
-    } else {
-        const new_item = preview_item.cloneNode(true);
-        new_item.removeAttribute("id");
-        new_item.className = "catalogue-item";
-        new_item.dataset.price = form_price.value;
-        new_item.dataset.name = form_name.value;
-        new_item.dataset.date = form_date.value;
-        new_item.dataset.type = form_type.value;
-        raw_items.push(new_item);
-        items.push(new CatalogueItem(new_item.dataset.name, new_item.dataset.price, new_item.dataset.type, new_item.dataset.date));
-        wrapper.append(new_item);
-        const res = await fetch("/create_page", {
-            method: "POST",
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify({
-                name: form_name.value,
-                price: form_price.value,
-                description: form_description.value,
+const express = require('express')
+const fs = require('fs');
+const path = require('path');
+const multer = require('multer');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "../Front")));
+app.use(cors());
+const upload = multer({ dest: '../Front/contents/' });
+
+app.post("/create_page", (req, res) => {
+    const {name, price, description} = req.body;
+    const src = path.join(__dirname, "../Front/HTML_pages/Item_pages/template.html");
+    const dst = path.join(__dirname, "../Front/HTML_pages/Item_pages/" + name + ".html");
+    fs.readFile(src, "utf-8", (err, data) => {
+        if (err) {
+            console.error(err);
+        }
+        let updated = data
+            .replace("{{NAME}}", name)
+            .replace("{{PRICE}}", price + "₽")
+            .replace("{{DESCRIPTION}}", description)
+            .replace("../../contents/Placeholder.png", "../../contents/" + name + ".png");
+        fs.writeFile(dst, updated, err => {
+            if (err) {
+                console.error(err);
+            }
+            res.json({
+                ok: true,
+                new_path: dst
             })
-        });
-        const formdata = new FormData();
-        const new_file = new File([form_image.files[0]], form_name.value, {type: form_image.files[0].type});
-        formdata.append('file', new_file);
-        formdata.append('name', form_name.value);
-        await fetch("/upload_item_image", {
-            method: "POST",
-            body: formdata
         })
-        const data = await res.json();
-        window.open("http://localhost:3000/HTML_Pages/Item_Pages/" + form_name.value + ".html");
-        console.log(data);
-    }
-}
+    })
+})
+app.post("/upload_item_image", upload.single("file"), (req, res) => {
+    const {name} = req.body;
+    const src = req.file.path;
+    const dst = path.join(__dirname, "../Front/contents/" + name + ".png");
+    console.log(dst)
+    fs.rename(src, dst, err => {
+        if (err) {
+            console.error(err);
+        }
+        res.json({ok: true});
+    })
+})
+app.listen(3000);
