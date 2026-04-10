a0eec264a98da561ce261f4f1089a785f27c58ec
LiquidSubstance <ivan.umn1k25@gmail.com>
2026-04-11 01:43:31 +0300

    Пока что неработающая связь с БД для загрузки товара на сервер

---
+++ Backend/upload_item.js
@@ -0,0 +1,50 @@
+const sqlite3 = require('sqlite3').verbose();
+const catalogue_db = new sqlite3.Database("../Data/catalogue.db");
+catalogue_db.run(`
+    CREATE TABLE IF NOT EXISTS items (
+        id          INTEGER PRIMARY KEY,
+        name        TEXT,
+        price       INTEGER,
+        date        TEXT,
+        description TEXT,
+        type        TEXT,
+        image_path  TEXT
+    )                   
+`);
+
+const express = require('express');
+const multer = require('multer');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.post("/upload_item", (req, res) => {
+    const { name, price, date, description, type, image_path} = req.body;
+
+    const sql = `
+        INSERT INTO items (name, price, date, description, type, image_path)
+        VALUES (?, ?, ?, ?, ?, ?)
+    `;
+
+    catalogue_db.run(sql, [name, price, date, description, type, image_path], function (err) {
+        res.json({
+            id: this.lastID,
+            name,
+            price,
+            date,
+            description,
+            type,
+            image_path
+        });
+    });
+})
+
+app.get("/upload_item", (req, res) => {
+    db.all("SELECT * FROM items ORDER BY id DESC", [], (err, rows) => {
+        res.json(rows);
+    })
+})
+
+app.listen(3000);
